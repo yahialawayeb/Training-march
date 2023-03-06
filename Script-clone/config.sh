1a6005866b316b9240312628ab57f862af39a341
yahialawayeb <lawayebyahia@gmail.com>
2023-03-06 13:56:57 +0100

    add script-clone-project

--- Script-clone/config.sh
+++ Script-clone/config.sh
@@ -7,7 +7,7 @@ USERNAME="yahialawayeb"
 REPO="Training-march"
 
 # SSH token
-SSHTOKEN="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"
+SSHTOKEN="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1a2z"
 
 # Local directory name
 LOCAL_DIR="/Users/yehya/Documents/Clone-Training/Training-march-dir"
