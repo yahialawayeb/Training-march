bb6d71ae36d88cecb2a46bd0bafe860a93fbc76f
Yahia.lawayeb <45715626+yahialawayeb@users.noreply.github.com>
2023-03-06 15:00:24 +0100

    Update config.sh

--- Script-clone/config.sh
+++ Script-clone/config.sh
@@ -4,6 +4,5 @@ USERNAME="yahialawayeb"
 # Repository name
 REPO="Training-march"
 
-token="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"
 # Local directory name
 LOCAL_DIR="/Users/yehya/Documents/Training-march-dir/Clone-Training"
